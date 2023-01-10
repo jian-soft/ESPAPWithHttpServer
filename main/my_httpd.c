0c567df036f7d70628904bc4920a548b68e47b22
jiansoft <jiansoft@yeah.net>
2023-01-10 21:51:20 +0800

    update to idf v5.0

--- main/my_httpd.c
+++ main/my_httpd.c
@@ -361,9 +361,9 @@ httpd_handle_t start_webserver(void)
     return NULL;
 }
 
-void stop_webserver(httpd_handle_t server)
+esp_err_t stop_webserver(httpd_handle_t server)
 {
     // Stop the httpd server
-    httpd_stop(server);
+    return httpd_stop(server);
 }
 
--- main/my_httpd.h
+++ main/my_httpd.h
@@ -10,7 +10,7 @@ extern "C" {
 
 
 httpd_handle_t start_webserver(void);
-void stop_webserver(httpd_handle_t server);
+esp_err_t stop_webserver(httpd_handle_t server);
 
 
 
